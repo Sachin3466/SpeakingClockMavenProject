dda7cbb5e89abbabb716ba0de4f82d631c846ad4
sachin biradar <sachin biradar@DESKTOP-T839NS9>
2023-07-25 01:04:40 +0530

    my commit

--- SpeakingClockMavenProject/src/main/java/com/demo/controller/SpeakingClockController.java
+++ SpeakingClockMavenProject/src/main/java/com/demo/controller/SpeakingClockController.java
@@ -1,13 +1,10 @@
 package com.demo.controller;
 
-import java.time.LocalTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.service.SpeakingClockService;
